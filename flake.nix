3740140adc36ab1227c251c9a85ad411caed9dee
godjoker379 <namineshkamarapu@gmail.com>
2025-11-03 19:47:44 +0530

    Update home-manager configuration username

--- flake.nix
+++ flake.nix
@@ -95,7 +95,7 @@
     # Standalone home-manager configuration entrypoint
     # Available through 'home-manager --flake .#your-username@your-hostname'
     homeConfigurations = {
-      "prabhas@synthesis" = home-manager.lib.homeManagerConfiguration {
+      "naminesh@synthesis" = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
         extraSpecialArgs = {inherit inputs outputs;};
 
