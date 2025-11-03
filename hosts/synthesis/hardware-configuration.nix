7196fa61d41e7f90243cec637e4176cde96619dc
godjoker379 <namineshkamarapu@gmail.com>
2025-11-03 19:58:19 +0530

    Update availableKernelModules for initrd

--- hosts/synthesis/hardware-configuration.nix
+++ hosts/synthesis/hardware-configuration.nix
@@ -12,7 +12,7 @@
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
 
-  boot.initrd.availableKernelModules = ["xhci_pci" "ahci" "nvme" "usbhid" "rtsx_usb_sdmmc"];
+  boot.initrd.availableKernelModules = ["vmd" "xhci_pci" "ahci" "nvme" "usb_storage" "sd_mod"];
   boot.initrd.kernelModules = [];
   boot.kernelModules = ["kvm-intel"];
   boot.extraModulePackages = [];
